07264b48a649cc7c7f956ea26aa7e6e2112d7827
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-03-02 20:45:44 +0200

    chore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import  App  from 'components/App';
-import { persistor, store } from 'redux/store';
+import { store } from 'redux/store';
 import './index.css';
 import { Provider } from 'react-redux';
 
